2cdb36ecb514b051aa2e54d68dda753274136c09
Yigit Suoglu <yigitsuoglu@sabanciuniv.edu>
2017-02-22 11:31:53 +0000

    Working STC gain model

--- stc.m
+++ stc.m
@@ -1,7 +1,5 @@
+% Begum, FM, Yarkin, Yigit
+% This file models a STC algoritm for sea clutter
 function out = stc(power, coef, max_dis, dis)
-    if(dis > max_dis)
-        out = 1;
-    else
         out = 1 - (coef * (max_dis - dis).^power);
-    end
 end
--- stc_main.m
+++ stc_main.m
@@ -1,24 +1,34 @@
 % Begum, FM, Yarkin, Yigit
-% This file models a STC for sea clutter
-increment = 0.1;
-max_see = 10;
-sea_ground = 5;
+% This file models a STC system for sea clutter
+waveSpeed = 299792.458; % speed of wave (km/s)
+sampleNumber = 100000; % number of samples to applied to 
+max_see = 146.38303661; % (2/4096)*waveSpeed; % range of radar (km)
+increment = 0.001463830361; % max_see / sampleNumber; % list of sample kms
+% sz = 100001; %  (max_see * increment + 1);
+sea_ground = 5; % ensure entance of while
 while(sea_ground ~= 0 && sea_ground ~= 1) % input check (1 or 0)
     sea_ground = input('1 for ground 0 for sea: ');
 end
-att_zero = 5;
+att_zero = 5; % ensure entance of while
 while(att_zero > 1 || att_zero < 0) % input check, attenuation must be between (0-1) 
     att_zero = input('Attenuation at zero point: ');
 end
-max_dis = -5;
+max_dis = -5; % ensure entance of while
 while(max_dis < 0) % input check,distance must be positive
-    max_dis = input('Maximum distance: ');
+    max_dis = input('Maximum distance (in km): ');
 end
   
-power = 2*(sea_ground + 1); % set power 4 for ground, 2 for sea
-coef = ((1 - att_zero) / (max_dis.^power)); % coefficent of gain
-dis = 0:increment:max_see;
-out = zeros(max_see * increment + 1, 'distributed');
-for i = 0:increment:max_see
-%   out[i] =  stc(power, coef, max_dis, dis[i]);
+pwr = 2*(sea_ground + 1); % set power 4 for ground, 2 for sea
+coef = ((1 - att_zero) / (max_dis.^pwr)); % coefficent of gain
+dis = 0:increment:max_see; % store sample distances
+
+%out = ones(1, 100001); % initialize filter values
+filtered =  stc(power, coef, max_dis, dis); % apply each element in to filter
+
+eNMax = uint64(max_dis / increment);
+
+for i = eNMax:100001
+   filtered(1,i) = 1;
 end
+
+plot(dis, filtered);
