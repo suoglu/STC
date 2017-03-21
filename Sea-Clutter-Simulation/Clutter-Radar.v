9d6d33a30a0035aa5fdab4fa062c1ab35a8dde51
Yigit Suoglu <yigitsuoglu@icloud.com>
2017-03-22 00:43:40 +0200

    Update Clutter-Radar.v

--- Sea-Clutter-Simulation/Clutter-Radar.v
+++ Sea-Clutter-Simulation/Clutter-Radar.v
@@ -1,2 +1,11 @@
 // This module simulates a sea clutter and radar interface
+// 50MHz clock freq
 
+module radar(arp, acp, trig, rst, clk)
+input clk, rst;
+output reg acp, trig;
+output arp;
+
+
+
+endmodule
