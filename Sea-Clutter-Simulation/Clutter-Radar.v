e0748860fb5eda5d944ce5f367e04e29cfcb0f67
Yigit Suoglu <yigitsuoglu@icloud.com>
2017-03-23 15:36:55 +0000

    Master trigger

--- Sea-Clutter-Simulation/Clutter-Radar.v
+++ Sea-Clutter-Simulation/Clutter-Radar.v
@@ -5,11 +5,11 @@
 // This module simulates sea clutter and radar interface
 module radar(arp, acp, trig, rst, clk);
   input clk, rst; //clk feq is 50 MHz, period is 20ns
-  output reg trig; //Master Trigger signal
+  output trig; //Master Trigger signal
   output arp, acp; //Azimuth Reset Pulse, Azimuth Change Pulse
   reg [11:0] acpState; //for 4096 angles
   wire clk_ACP; //Clock signal for acp
-  //period equal to time between ACP signals (~488.280ns, 24412 cycle @ 50Mhz)
+  //clk_ACP period equal to time between ACP signals (~488.280ns, 24412 cycle @ 50Mhz)
 
 
   assign arp = (acpState == 12'b0) & acp; //ARP is high only when ACP is high and ACP counter is at zero
@@ -28,7 +28,12 @@ module radar(arp, acp, trig, rst, clk);
   acpClkgen_half acpC(.rst(rst), .clk(clk), .clk_ACP(clk_ACP)); //for 50% duty cycle of ACP
 //acpClkgen_one acpC(.rst(rst), .clk(clk), .clk_ACP(clk_ACP)); //for 1% duty cycle of ACP
 
+  master_triger mtig(.clk(clk), .rst(rst), .arp(arp), .trig(trig));
+
+
 endmodule
+
+
 
 //this module generates Clock signal for ACP
 //created clock has period of 488.280ns and duty cycle of 50%
@@ -86,5 +91,69 @@ module acpClkgen_one(rst, clk, clk_ACP);
       begin
         clk_ACP <= 0; //create negedge
       end
+    end
+endmodule
+
+
+//this module creates trigger signal
+//trigger waits ARP signal to be high at least once to start after a reset
+//parameters can be adjusted
+module master_triger(clk, rst, arp, trig);
+  parameter counterSize = 15; //register size for counter
+  parameter trigerPeriod = 15'd24412; //trigger period in cycles (or period/20ns)
+  parameter trigerHwidth = 15'd50; //cycle count (or period/20ns) of high tigger signal
+  //note: cycle count of low trigger signal = trigerPeriod - trigerH-width;
+
+  input clk, rst, arp; //control signals
+  output trig; //master trigger output
+  reg [(counterSize-1):0] counter; //counter
+  reg trig_en; //master trigger is enabled
+  reg trig_reg;
+
+  always@(posedge clk or posedge rst)
+    begin
+    //counter control
+      if(rst)
+        begin
+          counter <= 0;
+        end
+      else if(counter == trigerPeriod) //if period is done
+        begin
+          counter <= 0; //reset counter
+        end
+      else if(trig_en) //if trigger is enabled
+        begin
+          counter <= counter + 1; //count up
+        end
+    end
+
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst) //if reset
+        begin  //disenable trigger
+          trig_en <= 0;
+        end
+      else if(arp) //if arp is high
+        begin //enable counter
+          trig_en <= 1;
+        end
+    end
+
+  assign trig = (trig_en & trig_reg); //if counter is not enabled output is always low
+
+  always@(posedge clk or posedge rst) //trig_reg
+    begin
+      if(rst) //if reset
+        begin //set triger register
+          trig_reg <= 1;
+        end
+      else if(counter == trigerPeriod) //if period is ended
+        begin //set trigger register
+          trig_reg <= 1;
+        end
+      else if(counter == trigerHwidth) //if trigger high time ended
+        begin //reset trigger register
+          trig_reg <= 0;
+        end
     end
 endmodule
